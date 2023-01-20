a0b115ccb271edb2a485e99973af908405c2c2bb
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2023-01-20 17:41:03 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Route
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
