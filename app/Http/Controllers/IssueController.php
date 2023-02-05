a22954c035ec735d45bbd0b0ba139728207e1e11
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2023-02-05 22:46:10 +0100

    update to app files

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -44,7 +44,7 @@ class IssueController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
