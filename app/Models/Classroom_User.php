ee462821601a09f91b27194fde8c02dad8d69922
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2023-02-14 22:02:30 +0100

    update app

--- app/Models/Classroom_User.php
+++ app/Models/Classroom_User.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Classroom_User extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "classroom_id",
+        "user_id"
+    ];
+
+    
 }
