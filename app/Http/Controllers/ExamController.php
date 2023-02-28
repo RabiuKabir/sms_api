a6581efbb5e703490b434071dccbeabd6985a07e
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2023-02-28 22:31:21 +0100

    update app

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -8,7 +8,7 @@ use App\Models\Exam;
 class ExamController extends Controller
 {
     /**
-     * Display a listing of the resource
+     * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
