15e210cc38d9b86929c04f70311c53616b371f0e
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2023-03-03 22:46:06 +0100

    update app

--- app/Http/Middleware/EnsureUserHasRole.php
+++ app/Http/Middleware/EnsureUserHasRole.php
@@ -1,25 +1,50 @@
 <?php
- 
+
 namespace App\Http\Middleware;
- 
+
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
- 
+use Illuminate\Support\Facades\Auth;
+
+// class EnsureUserHasRole
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+//      */
+//     public function handle(Request $request, Closure $next,  string $role): Response
+//     {
+//         if (Auth::user()->$role) {
+//             return $next($request);
+//         }
+
+//         //     echo (Auth::user()->role);
+//         return response('You do not have permission to access this page', 404);
+//     }
+// }
+
+
 class EnsureUserHasRole
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next, string $role): Response
+    public function handle($request, Closure $next, ...$roles)
     {
-        if ($request->user()->$role) {
-            return $next($request);
+        $roleIds = ['admin' => 'admin', 'student' => 'student', 'teacher' => 'teacher'];
+        $allowedRoleIds = [];
+        foreach ($roles as $role) {
+            if (isset($roleIds[$role])) {
+                $allowedRoleIds[] = $roleIds[$role];
+            }
         }
- 
+        $allowedRoleIds = array_unique($allowedRoleIds);
+
+        if (Auth::check()) {
+            if (in_array(Auth::user()->role, $allowedRoleIds)) {
+                return $next($request);
+            }
+        }
         return response('You do not have permission to access this page', 404);
+        //     return redirect('/');
     }
- 
 }
