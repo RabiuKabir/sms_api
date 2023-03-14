21a78702ede51f31e566ec9ed8a07bb688adc32f
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2023-03-14 22:15:20 +0100

    update app

--- app/Http/Middleware/allowStudent.php
+++ app/Http/Middleware/allowStudent.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 class allowStudent
 {
     /**
-     * Handle an incoming request.
+     * Handle an incoming request
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
